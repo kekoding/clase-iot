d25863b858672622492269ebb039a6b779a83934
Jorge <keko_qt@icloud.com>
2023-11-06 21:12:43 -0600

    clase 2023-11-06

--- 2023-11-06/index.js
+++ 2023-11-06/index.js
@@ -1,2 +1,28 @@
-const h1 = document.getElementById("encabezado");
-h1.innerHTML = "hola";
+//const = variable constante, no se puede cambiar
+const msg = "El mensaje";
+// var = variable global que puede ser cambiada
+var msg2 = "El Mensaje 2";
+// let = variables locales que pueden ser cambiadas
+let msg3 = "El Mensaje 3";
+
+function encenderLed(msg) {
+  console.log(`El mensaje es ${msg}`);
+  return;
+}
+
+var contador = 0;
+function incrementarConteo(contador) {
+  contador++;
+  console.log(`El valor del contador es ${contador}`);
+}
+
+("Este es un string");
+"Este es otro string"`Este es todavía otro string`;
+
+let arregloNumeros = [1, 2, 3, 4, 5];
+arregloNumeros.forEach((numero) => console.log(numero * numero));
+
+arregloNumeros.forEach((numero) => {
+  let cuadrado = numero * numero;
+  console.log(cuadrado);
+});
